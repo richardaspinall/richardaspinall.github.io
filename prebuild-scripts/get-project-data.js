0074eb5ef0f6cd8c4cdda517bd457400bbd776e6
richie912 <rls.aspinall@gmail.com>
2021-02-21 12:49:36 +1100

    Updated project script

--- prebuild-scripts/get-project-data.js
+++ prebuild-scripts/get-project-data.js
@@ -1,6 +1,20 @@
 const https = require('https');
 const fs = require('fs');
 
+const user = 'richardaspinall';
+
+const projectsToAdd = [
+  {
+    name: 'confusion-website',
+  },
+  {
+    name: 'pingpong-css',
+  },
+  {
+    name: 'slack-api-feature-breakdowns',
+  },
+];
+
 async function sendRequest(options) {
   let p = new Promise((resolve, reject) => {
     const req = https.request(options, (res) => {
@@ -25,62 +39,48 @@ async function sendRequest(options) {
   return await p;
 }
 
-async function getProjectDataFromGithub() {
-  const allRepoOptions = {
-    hostname: 'api.github.com',
-    path: '/users/richardaspinall/repos',
-    headers: {
-      'User-Agent': 'richardaspinall.github.io',
-      Accept: 'application/vnd.github.v3+json',
-    },
-    method: 'GET',
-  };
-
-  const repoOptions = {
-    hostname: 'api.github.com',
-    path: '/repos/richardaspinall/richardaspinall.github.io',
-    headers: {
-      'User-Agent': 'richardaspinall.github.io',
-      Accept: 'application/vnd.github.v3+json',
-    },
-    method: 'GET',
-  };
-
-  const topicOptions = {
-    hostname: 'api.github.com',
-    path: '/repos/richardaspinall/richardaspinall.github.io/topics',
-    headers: {
-      'User-Agent': 'richardaspinall.github.io',
-      Accept: 'application/vnd.github.mercy-preview+json',
-    },
-    method: 'GET',
-  };
-
-  let allRepos = await sendRequest(allRepoOptions);
-
-  let projects = [];
-  for (let index = 0; index < allRepos.length; index++) {
-    topicOptions.path = `/repos/richardaspinall/${allRepos[index].name}/topics`;
-    const topics = await sendRequest(topicOptions);
-    const project = {
-      name: allRepos[index].name,
-      description: allRepos[index].description,
-      githubUrl: allRepos[index].html_url,
-      readme: allRepos[index].html_url + '/blob/main/README.md',
-      homepage: allRepos[index].homepage,
-      topics: topics,
-    };
-    if (topics.names.includes('test')) {
-      console.log(`Not adding test project: ${project.name}`);
-    } else {
-      projects.push(project);
+function search(nameKey, myArray) {
+  for (var i = 0; i < myArray.length; i++) {
+    if (myArray[i].name === nameKey) {
+      return myArray[i];
     }
   }
+}
 
-  fs.writeFile('../_data/project-data.json', JSON.stringify(projects), function (err) {
+async function getProjectsFromGithub() {
+  const options = {
+    hostname: 'api.github.com',
+    path: `/users/${user}/repos`,
+    headers: {
+      'User-Agent': `${user}.github.io`,
+      Accept: 'application/vnd.github.v3+json',
+    },
+    method: 'GET',
+  };
+  const allRepos = await sendRequest(options);
+  let projectsToWrite = [];
+
+  for (let index = 0; index < projectsToAdd.length; index++) {
+    const projectToAdd = search(projectsToAdd[index].name, allRepos);
+    options.path = `/repos/${user}/${projectToAdd.name}/topics`;
+    options.headers.Accept = 'application/vnd.github.mercy-preview+json';
+    const topics = await sendRequest(options);
+    const project = {
+      name: projectToAdd.name,
+      description: projectToAdd.description,
+      githubUrl: projectToAdd.html_url,
+      readme: projectToAdd.html_url + '/blob/main/README.md',
+      homepage: projectToAdd.homepage,
+      topics: topics,
+    };
+
+    projectsToWrite.push(project);
+  }
+
+  fs.writeFile('../_data/project-data.json', JSON.stringify(projectsToWrite), function (err) {
     if (err) throw err;
     console.log('Replaced!');
   });
 }
 
-getProjectDataFromGithub();
+getProjectsFromGithub();
